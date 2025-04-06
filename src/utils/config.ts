d4438a6b241841350237b3592c8d9d83a83ec276
Salimjon Rahimov <116638621+salimjon10@users.noreply.github.com>
2025-04-06 20:53:15 +0700

    fix config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 const config = {
   port: process.env.PORT || 3000,
   mongoURL: process.env.MONGO_URL || "mongodb://localhost:27017",
-  jwtSecret: process.env.JWT_SECRET || "your-secret-key", // Важно изменить в production!
+  jwtSecret: process.env.JWT_SECRET || "your-secret-key",
 };
 
 export default config;
